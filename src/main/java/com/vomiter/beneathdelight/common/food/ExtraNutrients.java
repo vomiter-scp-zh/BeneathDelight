beb3c915026522423acd87d175629468bcc559f3
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2026-08-20 22:53:36 +0800

    refactor dynamic food system

--- src/main/java/com/vomiter/beneathdelight/common/food/ExtraNutrients.java
+++ src/main/java/com/vomiter/beneathdelight/common/food/ExtraNutrients.java
@@ -2,44 +2,65 @@ package com.vomiter.beneathdelight.common.food;
 
 import com.soytutta.mynethersdelight.common.registry.MNDItems;
 import com.vomiter.beneathdelight.Helpers;
-import com.vomiter.survivorsdelight.adapter.cooking_pot.CookingPotExtraNutrientRules;
-import com.vomiter.survivorsdelight.util.SDUtils;
+import com.vomiter.survivorsdelight.adapter.cooking_pot.dynamic.CookingPotDynamicRules;
+import com.vomiter.survivorsdelight.adapter.cooking_pot.dynamic.DynamicFoodContext;
+import com.vomiter.survivorsdelight.util.FoodDataBuilder;
 import net.dries007.tfc.common.capabilities.food.FoodCapability;
+import net.dries007.tfc.common.capabilities.food.FoodData;
+import net.dries007.tfc.common.capabilities.food.IFood;
 import net.dries007.tfc.common.capabilities.food.Nutrient;
 import net.dries007.tfc.common.items.Food;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import java.util.Optional;
 
 public class ExtraNutrients {
     public static void onCommonSetup(FMLCommonSetupEvent event){
         event.enqueueWork(() -> {
-            CookingPotExtraNutrientRules.register(
-                    Helpers.id("stuffed_hoglin"),
-                    2000,
-                    new CookingPotExtraNutrientRules.SimpleRule(
-                            (level, stack, nutrient, data) ->
-                                    stack.is(MNDItems.RAW_STUFFED_HOGLIN.get())
-                                    && (nutrient.equals(Nutrient.PROTEIN) || nutrient.equals(Nutrient.VEGETABLES))
-                            , (level, stack, nutrient, data) -> {
-                                if(nutrient.equals(Nutrient.PROTEIN)) return data.protein() * 0.6f;
-                                else if(nutrient.equals(Nutrient.VEGETABLES)) return data.vegetables() * 1f;
-                                return 0;
-                            }
+            CookingPotDynamicRules.register(
+                    new CookingPotDynamicRules.RuleHolder(
+                            2000,
+                            Helpers.id("stuffed_hoglin"),
+                            ((food, context) -> {
+                                if (context.phase().equals(DynamicFoodContext.Phase.INDIVIDUAL)){
+                                    if (context.stack().is(MNDItems.RAW_STUFFED_HOGLIN.get())){
+                                        var stuffedHoglinFood = Optional.ofNullable(FoodCapability.get(context.stack())).map(IFood::getData).orElse(FoodData.EMPTY);
+                                        return food.addBuilder(FoodDataBuilder.from(stuffedHoglinFood).mulNutrient(1.6f, Nutrient.PROTEIN).mulNutrient(3, Nutrient.VEGETABLES));
+                                    }
+                                }
+                                return food;
+                            })
                     )
             );
 
-            CookingPotExtraNutrientRules.register(
-                    Helpers.id("ghast_sourdough"),
-                    2000,
-                    new CookingPotExtraNutrientRules.SimpleRule(
-                            (level, stack, nutrient, data) ->
-                                    stack.is(MNDItems.GHAST_SOURDOUGH.get())
-                                            && (nutrient.equals(Nutrient.GRAIN))
-                            , (level, stack, nutrient, data) -> {
-                        if(nutrient.equals(Nutrient.GRAIN))
-                            return FoodCapability.get(new ItemStack(SDUtils.getTFCFoodItem(Food.WHEAT_BREAD))).getData().grain();
-                        return 0;
-                    })
+            CookingPotDynamicRules.register(
+                    new CookingPotDynamicRules.RuleHolder(
+                            2000,
+                            Helpers.id("ghast_sourdough"),
+                            ((food, context) -> {
+                                if (context.phase().equals(DynamicFoodContext.Phase.INDIVIDUAL)){
+                                    if (context.stack().is(MNDItems.GHAST_SOURDOUGH.get())){
+                                        return food.addData(CookingPotDynamicRules.getBuiltInFoodData(Food.WHEAT_BREAD));
+                                    }
+                                }
+                                return food;
+                            })
+                    )
+            );
+
+            CookingPotDynamicRules.register(
+                    new CookingPotDynamicRules.RuleHolder(
+                            1000,
+                            Helpers.id("bullet_pepper"),
+                            ((food, context) -> {
+                                if (context.phase().equals(DynamicFoodContext.Phase.INDIVIDUAL)){
+                                    if (context.stack().is(MNDItems.BULLET_PEPPER.get())){
+                                        return food.mulNutrient(1.3f, Nutrient.VEGETABLES, Nutrient.DAIRY, Nutrient.FRUIT);
+                                    }
+                                }
+                                return food;
+                            })
+                    )
             );
 
         });
